aa81e649bb9b5f9bb104dca533f513b4f1827367
Tuan Ha <hatuan@email.cz>
2023-12-02 23:12:15 +0100

    display books in myLibrary, add css styling to book cards

--- script.js
+++ script.js
@@ -21,7 +21,45 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
+// A function that displays all books on the web
+function displayBooks() {
+    let libraryContainer = document.querySelector(".books-container");
+
+    for (let book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.className = "book-card"
+        
+        const author = document.createElement("p");
+        const title = document.createElement("p");
+        const numberOfPages = document.createElement("p");
+        const read = document.createElement("p");
+
+        author.textContent = book.author;
+        title.textContent = book.title;
+        numberOfPages.textContent = book.numberOfPages;
+        read.textContent = book.isRead;
+
+        bookCard.appendChild(author);
+        bookCard.appendChild(title);
+        bookCard.appendChild(numberOfPages);
+        bookCard.appendChild(read);
+
+        libraryContainer.appendChild(bookCard);
+    }
+}
+
 // Dummy Books
 const snowMan = new Book("Jo Nesbo", "The Snowman", 400, true);
 const tokyoGhoul = new Book("Sui Ishida", "Tokoy Ghoul", 100, false);
 const wimpyKid = new Book("Jeff Kinney", "Diary of a Wimpy Kid", 300, true);
+const test = new Book("Jo Nesbo", "The Snowman", 400, true);
+const test2 = new Book("Jo Nesbo", "The Snowman", 400, true);
+
+myLibrary.push(snowMan);
+myLibrary.push(tokyoGhoul);
+myLibrary.push(wimpyKid);
+myLibrary.push(test);
+myLibrary.push(test2);
+
+// firing functions
+displayBooks();
